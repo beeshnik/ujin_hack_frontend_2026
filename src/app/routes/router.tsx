655616fc7c1648c1bc49eeb50573a46a9c46953f
Михаил <mishafedoseev753@gmail.com>
2026-06-06 15:35:39 +0500

    feat: вставил из пета свои компоненты

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,11 +1,12 @@
 
-import { createBrowserRouter } from 'react-router-dom'
-// import { ROUTES } from '@/shared/config/route-paths'
-// import { RequireAuth } from './guards/require-auth'
-// import { RequirePermission } from './guards/require-permission'
-// import { AppShell } from '@/widgets/app-shell/ui/app-shell'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { ROUTES } from '@/shared/config/route-paths'
+import { RequireAuth } from './guards/require-auth'
+import { RequirePermission } from './guards/require-permission'
+import { AppShell } from '@/widgets/app-shell/ui/app-shell'
+import { lazy, Suspense } from 'react'
 
-// const LoginPage = lazy(() => import('@/pages/auth/login'))
+const LoginPage = lazy(() => import('@/pages/auth/login'))
 // const DashboardPage = lazy(() => import('@/pages/dashboard'))
 // const TablesPage = lazy(() => import('@/pages/tables'))
 // const ChartsPage = lazy(() => import('@/pages/charts'))
@@ -13,98 +14,90 @@ import { createBrowserRouter } from 'react-router-dom'
 // const PanelsPage = lazy(() => import('@/pages/panels'))
 // const UiKitPage = lazy(() => import('@/pages/ui-kit'))
 // const SettingsPage = lazy(() => import('@/pages/settings'))
-// const AccessDeniedPage = lazy(() => import('@/pages/access-denied'))
-// const NotFoundPage = lazy(() => import('@/pages/not-found'))
+const AccessDeniedPage = lazy(() => import('@/pages/access-denied'))
+const NotFoundPage = lazy(() => import('@/pages/not-found'))
 
-// const PageLoader = () => (
-//   <div className="flex min-h-[200px] items-center justify-center">
-//     <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-//   </div>
-// )
-
-// function S(Component: React.ComponentType) {
-//   return (
-//     <Suspense fallback={<PageLoader />}>
-//       <Component />
-//     </Suspense>
-//   )
-// }
-
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <div>Sss</div>
-    }
-  ]
+const PageLoader = () => (
+  <div className="flex min-h-[200px] items-center justify-center">
+    <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+  </div>
 )
 
-// export const router = createBrowserRouter([
-//   {
-//     path: ROUTES.LOGIN,
-//     element: S(LoginPage),
-//   },
-//   {
-//     path: '/',
-//     element: <Navigate to={ROUTES.DASHBOARD} replace />,
-//   },
-//   {
-//     path: ROUTES.ACCESS_DENIED,
-//     element: S(AccessDeniedPage),
-//   },
-//   {
-//     element: (
-//       <RequireAuth>
-//         <AppShell />
-//       </RequireAuth>
-//     ),
-//     children: [
-//       {
-//         path: ROUTES.DASHBOARD,
-//         element: (
-//           <RequirePermission permission="dashboard:view">{S(DashboardPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.TABLES,
-//         element: (
-//           <RequirePermission permission="users:view">{S(TablesPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.CHARTS,
-//         element: (
-//           <RequirePermission permission="analytics:view">{S(ChartsPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.FORMS,
-//         element: (
-//           <RequirePermission permission="users:view">{S(FormsPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.PANELS,
-//         element: (
-//           <RequirePermission permission="users:view">{S(PanelsPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.UI_KIT,
-//         element: (
-//           <RequirePermission permission="ui-kit:view">{S(UiKitPage)}</RequirePermission>
-//         ),
-//       },
-//       {
-//         path: ROUTES.SETTINGS,
-//         element: (
-//           <RequirePermission permission="settings:view">{S(SettingsPage)}</RequirePermission>
-//         ),
-//       },
-//     ],
-//   },
-//   {
-//     path: ROUTES.NOT_FOUND,
-//     element: S(NotFoundPage),
-//   },
-// ])
+function S(Component: React.ComponentType) {
+  return (
+    <Suspense fallback={<PageLoader />}>
+      <Component />
+    </Suspense>
+  )
+}
+
+
+export const router = createBrowserRouter([
+  {
+    path: ROUTES.LOGIN,
+    element: S(LoginPage),
+  },
+  {
+    path: '/',
+    element: <Navigate to={ROUTES.DASHBOARD} replace />,
+  },
+  {
+    path: ROUTES.ACCESS_DENIED,
+    element: S(AccessDeniedPage),
+  },
+  {
+    element: (
+      <RequireAuth>
+        <AppShell />
+      </RequireAuth>
+    ),
+    // children: [
+    //   {
+    //     path: ROUTES.DASHBOARD,
+    //     element: (
+    //       <RequirePermission permission="dashboard:view">{S(DashboardPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.TABLES,
+    //     element: (
+    //       <RequirePermission permission="users:view">{S(TablesPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.CHARTS,
+    //     element: (
+    //       <RequirePermission permission="analytics:view">{S(ChartsPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.FORMS,
+    //     element: (
+    //       <RequirePermission permission="users:view">{S(FormsPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.PANELS,
+    //     element: (
+    //       <RequirePermission permission="users:view">{S(PanelsPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.UI_KIT,
+    //     element: (
+    //       <RequirePermission permission="ui-kit:view">{S(UiKitPage)}</RequirePermission>
+    //     ),
+    //   },
+    //   {
+    //     path: ROUTES.SETTINGS,
+    //     element: (
+    //       <RequirePermission permission="settings:view">{S(SettingsPage)}</RequirePermission>
+    //     ),
+    //   },
+    // ],
+  },
+  {
+    path: ROUTES.NOT_FOUND,
+    element: S(NotFoundPage),
+  },
+])
